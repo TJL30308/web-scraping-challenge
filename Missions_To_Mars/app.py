e9e0db1fa05c677c5a8d8ae80b301a31cc2c4d83
Topias Lemetyinen <topias.lemetyinen@gmail.com>
2020-03-09 16:23:34 -0400

    updates

--- Missions_To_Mars/app.py
+++ Missions_To_Mars/app.py
@@ -4,7 +4,7 @@ import mission_to_mars
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/weather_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
