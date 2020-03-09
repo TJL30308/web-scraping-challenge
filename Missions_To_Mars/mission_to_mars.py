20d9afe0db6be8d2261d7e4f34a0581f94555e11
Topias Lemetyinen <topias.lemetyinen@gmail.com>
2020-03-09 16:39:34 -0400

    updates

--- Missions_To_Mars/mission_to_mars.py
+++ Missions_To_Mars/mission_to_mars.py
@@ -19,11 +19,9 @@ def init_browser():
 # Latest News Headline and Body Scraping
 def scrape_news():
 
-    Browser = init_browser()
     url = 'https://mars.nasa.gov/news/'
-    Browser.visit(url)
-    html = Browser.html
-    soup = BeautifulSoup(html, 'html.parser')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
     headline = soup.find('div', class_='content_title').find('a').text
     body = soup.find('div', class_='rollover_description_inner').text
 
